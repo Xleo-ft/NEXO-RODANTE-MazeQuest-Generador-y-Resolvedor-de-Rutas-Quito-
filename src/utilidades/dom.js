5fba963d215baae96e4edefbcc5aebd70e7ab58d
ccuascota-beep <carloscuascota2007@gmail.com>
2026-08-29 22:31:39 -0500

    Obtencion de los elementos que simplifica la busqueda de elementos HTML

---
+++ src/utilidades/dom.js
@@ -0,0 +1,3 @@
+// ===\r\n// UTILIDADES: helpers genéricos de acceso al DOM.\r\n// ===
+// Obtiene un elemento de la interfaz a partir de su identificador HTML.
+const obtenerElemento = (identificador) => document.getElementById(identificador);
